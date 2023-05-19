513b7ed3d9257580fb0a5f41a34430003f33d986
Guilherme444 <guilherme.caetano1010@gmail.com>
2023-05-19 12:30:57 -0300

    Componentes corretos

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
 
-const App = () => {
-
-  let time = new Date().toLocaleTimeString();
-
-  const [ctime, setCtime] = useState(time);
-
-  const UpdateTime = () => {
-    time = new Date().toLocaleTimeString();
-    setCtime(time);
-
-  };
-  setInterval(UpdateTime,1000)
-
-  return (
+function App() {
+ const  [value, setValue] = useState(0)
+ return(
     <>
-    <h1> {ctime} </h1>
+    <h2>Create a count in react</h2> 
+    <p>{value}</p>
+ <button type="button" className="btn" onClick={() => setValue(value-1)}>decrease</button>
+ <button type="button" className="btn" onClick={() => setValue(0)}>reset</button>
+ <button type="button" className="btn" onClick={() => setValue(value+1)}>increase</button>
     </>
-  )
+ )
 }
-
 export default App
